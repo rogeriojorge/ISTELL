257f376a63e07dbc74c281b07d83a7b764c4d093
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-03-15 20:17:50 +0000

    slightly more quasisymmetric stellarator

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ except ImportError:
     pprint = print
 ######## INPUT PARAMETERS ########
 ncoils=7
-CS_THRESHOLD = 0.0028
-CS_WEIGHT = 1e32
+CS_THRESHOLD = 0.002
+CS_WEIGHT = 1e33
 max_nfev = 30
-iota_target = 0.187
+iota_target = 0.137
 iota_weight = 5e1
-aspect_target = 7.0
-aspect_weight = 1e-3
-max_modes = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
+aspect_target = 8.0
+aspect_weight = 1e-2
+quasisymmetry_weight = 5e4
+max_modes = [1, 1, 2, 2, 3, 3, 4, 4]
 rel_step = 1e-5
 abs_step = 1e-7
 ISTTOK_R0 = 0.46
@@ -49,7 +50,7 @@ filename = os.path.join(os.path.dirname(__file__), 'input.nfp2_initial')
 vmec = Vmec(filename, mpi=mpi, verbose=False, ntheta=ntheta_VMEC, nphi=nphi_VMEC, range_surface='half period')
 s = vmec.boundary
 ### Create coils
-base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=ISTTOK_R0, R1=ISTTOK_R1, order=1, numquadpoints=numquadpoints)
+base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=ISTTOK_R0, R1=ISTTOK_R1, order=2, numquadpoints=numquadpoints)
 #base_currents = [Current(1e5) for i in range(ncoils)]
 #base_currents[0].fix_all()
 #coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
@@ -68,7 +69,7 @@ for max_mode in max_modes:
     surf.fixed_range(mmin=0, mmax=max_mode, nmin=-max_mode, nmax=max_mode, fixed=False)
     surf.fix("rc(0,0)")  # Major radius
     prob = LeastSquaresProblem.from_tuples([(vmec.aspect, aspect_target, aspect_weight),
-                                            (qs.residuals, 0, 5e0),
+                                            (qs.residuals, 0, quasisymmetry_weight),
                                             (vmec.mean_iota, iota_target, iota_weight),
                                             (Jcsdist.J, 0, CS_WEIGHT)])
     pprint("Iota before optimization:", vmec.mean_iota())
@@ -92,15 +93,20 @@ for max_mode in max_modes:
     # vmec.indata.ftol_array[:3]  = [1e-14, 1e-14, 1e-14]
     vmec.write_input(f'input.ISTTOK_maxmode{max_mode}')
 ### Write result
-vmec.indata.ns_array[:3]    = [  16,    51,    101]
-vmec.indata.niter_array[:3] = [ 2000,  3000, 20000]
-vmec.indata.ftol_array[:3]  = [1e-14, 1e-14, 1e-14]
+vmec.indata.ns_array[:5]    = [  16,     51,   101,   151,   201]
+vmec.indata.niter_array[:5] = [ 2000,  2000,  2000,  2000, 20000]
+vmec.indata.ftol_array[:5]  = [1e-14, 1e-14, 1e-14, 1e-14, 1e-15]
 vmec.write_input(f'input.ISTTOK_final')
-vmec = Vmec('input.ISTTOK_final', mpi=mpi, verbose=True, ntheta=ntheta_VMEC, nphi=nphi_VMEC, range_surface='half period')
+vmec = Vmec('input.ISTTOK_final', mpi=mpi, verbose=True, ntheta=ntheta_VMEC, nphi=nphi_VMEC*2*s.nfp)
 vmec.run()
-s = vmec.boundary
-s.to_vtk("surf_final")
 if mpi.proc0_world:
+    s = vmec.boundary
+    s.to_vtk("surf_final")
+    base_currents = [Current(1e5) for i in range(ncoils)]
+    base_currents[0].fix_all()
+    coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
+    curves = [c.curve for c in coils]
+    curves_to_vtk(curves, 'curves_final')
     shutil.move(f"wout_ISTTOK_final_000_000000.nc", f"wout_ISTTOK_final.nc")
     os.remove(f'input.ISTTOK_final_000_000000')
 # Remove spurious files
