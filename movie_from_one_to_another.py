ef692a95735fc1492ffd73ea4daa4bd2fe8d6692
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-03-12 16:10:15 -0500

    new movie

--- movie_from_one_to_another.py
+++ movie_from_one_to_another.py
@@ -12,7 +12,14 @@ from simsopt.geo import SurfaceRZFourier
 # Constants
 nphi = 60
 ntheta = 60
-N_interpolation = 10
+N_interpolation = 30
+N_extra = 2
+x_centered = 5
+def interp_function(x): return 1/(1+np.exp(-x+x_centered))
+interp_factors = sorted(np.concatenate([np.linspace(0,0.03,N_extra,endpoint=True),interp_function(np.linspace(0, x_centered*2, N_interpolation, endpoint=True)),np.linspace(0.97,1.0,N_extra,endpoint=True)]))
+# plt.plot(interp_factors)
+# plt.show()
+# exit()
 ncoils = 4
 output_path = 'surfs_between_nfp3'
 this_path = '/Users/rogeriojorge/local/ISTELL'
@@ -34,8 +41,12 @@ bs1 = load(os.path.join(coils_path, "biot_savart1.json"))
 bs2 = load(os.path.join(coils_path, "biot_savart2.json"))
 bs1_dofs = bs1.x
 bs2_dofs = bs2.x
-print(f"Coil currents 1: {[bs2.coils[i].current.get_value() for i in range(ncoils)]}")
-print(f"Coil currents 2: {[bs1.coils[i].current.get_value() for i in range(ncoils)]}")
+currents_1 = np.array([bs1.coils[i].current.get_value() for i in range(ncoils)])
+currents_2 = np.array([bs2.coils[i].current.get_value() for i in range(ncoils)])
+max_current = max(max(currents_1), max(currents_2))
+min_current = min(min(currents_1), min(currents_2))/max_current
+print(f"Coil currents 1: {currents_1}")
+print(f"Coil currents 2: {currents_2}")
 
 # Read coils mesh
 coils_vtu = pv.read(os.path.join(coils_path, "curves_opt_halfnfp.vtu"))
@@ -47,10 +58,10 @@ Path(output_path).mkdir(parents=True, exist_ok=True)
 image_files = []
 
 # Convert VTK files to PNG images and store file names
-interp_factors = np.linspace(0, 1, N_interpolation, endpoint=True)
-for i in range(2 * N_interpolation - 1):
-    j = 2 * N_interpolation - i - 2 if i > N_interpolation - 1 else i
+for i in range(2 * len(interp_factors) - 1):
+    j = 2 * len(interp_factors) - i - 2 if i > len(interp_factors) - 1 else i
     factor = interp_factors[j]
+    # print(factor)
 
     # Interpolate surfaces and currents
     surf_between.x = (1 - factor) * surf1_dofs + factor * surf2_dofs
@@ -67,39 +78,34 @@ for i in range(2 * N_interpolation - 1):
     surf_between_vtk = pv.read(vtk_file)
 
     plotter = pv.Plotter(off_screen=True)
-    surf_mesh = plotter.add_mesh(surf_between_vtk, scalars="B.n/B", cmap="coolwarm")
 
-    # Create a colormap
-    cmap = plt.cm.coolwarm
+    args_cbar = dict(height=0.1, vertical=False, position_x=0.29, position_y=0.03, color="k", title_font_size=24, label_font_size=16)
 
+    surf_mesh = plotter.add_mesh(surf_between_vtk, scalars="B.n/B", cmap="coolwarm", clim=[-0.03, 0.03], scalar_bar_args=args_cbar)
     # Normalize current values
     current_values = np.array([coil.current.get_value() for coil in bs1.coils])
-    normalized_currents = ((current_values - np.min(current_values)) / (np.max(current_values) - np.min(current_values)))
-
-    # Map normalized currents to colors
-    colors = [mcolors.rgb2hex(cmap(norm)) for norm in normalized_currents]
-
     for coil_index, coil in enumerate(bs1.coils):
         # Extract the points for the current coil
         coil_points = coils_vtu.extract_cells(coil_index)
         
         # Check if the coil has points before plotting
         if coil_points.n_points > 0:
-            # Get the color for the current coil based on its current value
-            color = colors[coil_index]
-
-            # Plot the current coil
-            plotter.add_mesh(coil_points, color=color, line_width=3)
+            args_cbar = dict(width=0.05, vertical=True, position_x=0.05, position_y=0.03, color="k", title_font_size=24, label_font_size=16, title='Current')
+            plotter.add_mesh(coil_points, line_width=6, label=f"Coil {coil_index}", scalars=[current_values[coil_index]/max_current]*coil_points.n_points, cmap="coolwarm", scalar_bar_args=args_cbar, clim=[min_current, 1])
+    # # Add scalar bar for the current values legend
+    # plotter.add_scalar_bar(title="Currents", vertical=True, width=0.05, position_x=0.05, position_y=0.1, title_font_size=24, label_font_size=16)
 
     # Set background to white
     plotter.set_background("white")
     
     # Adjust camera position (rotate and zoom)
-    plotter.camera_position = [(0, 1, 1), (0, 0, 0), (0, 0, 1)]  # Example camera position (adjust as needed)
+    plotter.camera_position = (-5.1,-0.9,0)#[(7, -2, 0), (0, 0, 0), (0, 0, 1)]  # Example camera position (adjust as needed)
+    # plotter.camera_clipping_range = [5, 20]  # Adjust the clipping range to zoom in
+    plotter.camera.zoom(1.6)
 
     plotter.show(screenshot=png_file)
     image_files.append(png_file)
-    exit()
+    # exit()
     
 # Create a gif from the PNG images
 gif_file = os.path.join(output_path, "surf_between_animation.gif")
@@ -110,3 +116,8 @@ with imageio.get_writer(gif_file, mode='I') as writer:
 
 # Print the path to the generated gif
 print(f"GIF created: {gif_file}")
+
+# Remove the VTS and PNG files
+for vtk_file in os.listdir(output_path):
+    if vtk_file.endswith(".vts") or vtk_file.endswith(".png"):
+        os.remove(os.path.join(output_path, vtk_file))
